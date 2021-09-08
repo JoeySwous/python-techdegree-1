a4c8f3adad5531956b624e20d2cb0e3328106c5b
JoeySwous <85312050+JoeySwous@users.noreply.github.com>
2021-09-07 21:03:26 -0500

    Add files via upload

---
+++ guessing_game.py
@@ -0,0 +1,66 @@
+"""
+Python Web Development Techdegree
+Project 1 - Number Guessing Game
+--------------------------------
+"""
+
+import random
+
+def start_game():
+    print('''  
+==============================================  
+
+  /\ /V\    
+|~||~||~|  
+| || || |  QUICK! THERE HAS BEEN A BOMB FOUND!
+|(~)T(~)|   
+=/1  10\=  THE ONLY WAY TO DEFUSE IT IS WITH A
+|| _.  ||  NUMBER 1 - 10! START NOW!
+| \/  / |
+|_/^^^\_|
+
+==============================================
+''')
+    high_score = 0
+    while True:
+        if high_score == False:
+            print('THERE IS NO HIGH SCORE YET.\n')
+        else:
+            print(f'HIGH SCORE: {high_score}\n')
+        random_number = random.randint(1, 10)
+        tries = 0
+        print('ENTER A NUMBER 1 - 10 TO BEGIN. ')        
+        while True:
+            try:
+                guess = int(input(">  "))
+                if guess < 1 or guess > 10:
+                    print('THAT NUMBER IS OUTSIDE OF THE RANGE.\nPLEASE ENTER ONLY NUMBERS 1 - 10.\n')
+                    tries +=1
+                    continue
+                if guess < random_number:
+                    print('IT\'S HIGHER. TRY AGAIN.\n')
+                    tries += 1
+                    continue
+                if guess > random_number:
+                    print('IT\'S LOWER. TRY AGAIN.\n')
+                    tries += 1
+                    continue
+                if guess == random_number:
+                    print(f'YOU\'RE A HERO!\nIT TOOK YOU {tries} TRIES TO GUESS {random_number}.\nYOU SUCCESSFULLY DEFUSED THE BOMB.\n')
+                    break
+                break
+            except ValueError:
+                print('SORRY! YOU ENTERED AN INCORRECT VALUE. TRY AGAIN WITH A NUMBER 1 - 10 ')
+                tries += 1
+                continue
+                
+        if high_score == 0 or tries < high_score:
+            high_score = tries
+        replay = input('WOULD YOU LIKE TO PLAY AGAIN?\nENTER Y TO PLAY AGAIN.  >> ')
+        if replay == 'Y'.lower():
+            continue
+        else:
+            print('UNTIL NEXT TIME THEN... ')
+            break                
+            
+start_game()
